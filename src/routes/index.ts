dcc1ccd6416a013165aad90124ebda325de579b6
Sherif Nada <snadalive@gmail.com>
2024-01-24 14:15:31 -0600

    dep inject router with firebase client, fix unit tests

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,202 +8,194 @@ import path from 'path';
 import { HOSTING_BUCKET_NAME } from '../aws/constants.js';
 import { ValidationError } from './errors.js';
 import { UploadedFile } from 'express-fileupload';
-import { readFileSync } from 'fs';
-// import * as admin from 'firebase-admin';
-// const admin = (await import('firebase-admin')).default;
-import {initializeApp, cert} from 'firebase-admin/app';
+import {App} from 'firebase-admin/app';
 import {getAuth} from 'firebase-admin/auth';
 
-const firebaseKey = JSON.parse(readFileSync(path.join(process.cwd(), 'secrets/firebase-svcaccount-key.json')).toString());
-const firebaseApp = initializeApp({
-    credential: cert(firebaseKey),
-});
-
-const router = Router();
-
-function parseBearerToken(token: string): string | undefined {
-    const split = token?.split(" ");
-    if (split && split.length === 2){
-        return split[1];
-    }
-
-    return undefined;
-}
-
-async function validateUserSignedIn(req, res, next) {
-    const idToken = parseBearerToken(req.headers?.authorization);
-    if (!idToken){
-        res.status(401).send({error: "auth", message: "Expected a bearer token"});
-        return;
-    }
-
-    try {
-        // TODO unit test this
-        const auth = getAuth(firebaseApp);
-        const decodedToken  = await auth.verifyIdToken(idToken);
-        req.userToken = decodedToken;
-        next();
-    } catch(error){
-        res.status(401).send({error: "auth", message: "Invalid auth token"})
-    }
-}
-
-router.post('/create_site', validateUserSignedIn, async (req: Request, res) => {
-    // TODO add API contract somewhere as middleware, this validation is nuts
-    try {
-        // TODO replace this with specific origins
-        // validate the uploaded file is a zip file
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        validateSubdomain(req.body);
-        assertZipFile(req);
-
-        // in zipFile, find the content root i.e the shallowest directory which contains an index.html file. 
-        // If no such directory exists then raise a ValidationError        
-        const zipFile = new AdmZip((req.files.zipFile as UploadedFile).data);
-        const contentRoot: string = inferContentRoot(zipFile);
-        console.log(`contentRoot: ${contentRoot}`);
-        
-        const subdomain = req.body.subdomain;
-        const bucketSiteUrl = `https://${subdomain}.litehost.io`;
-        const tmpDir = unzipToTmpDir(zipFile);
-        const uploadRoot = path.join(tmpDir, contentRoot);
-        await uploadDir(HOSTING_BUCKET_NAME, subdomain, uploadRoot, uploadRoot);
-        res.status(200).send({message: `Site created at ${bucketSiteUrl}`, websiteUrl: bucketSiteUrl});
-    } catch (error) {
-        if (error instanceof ValidationError){
-            res.status(error.status).send(error.body);
-        } else {
-            console.error(error);
-            res.status(500).send('An error occurred while creating the site');
+const createRouter = (firebaseApp: App) => {
+    const router = Router();
+    function parseBearerToken(token: string): string | undefined {
+        const split = token?.split(" ");
+        if (split && split.length === 2){
+            return split[1];
         }
+    
+        return undefined;
     }
-});
-
-router.get('*', async(req, res, next) => {
-    try {
-        const subdomain = extractSubdomainFromHost(req.headers.host);
-        if (subdomain){
-            let objectPath = req.originalUrl.split("?")[0];
-            if (!isRequestForFile(objectPath) && objectPath.endsWith("/")){
-                objectPath = path.join(objectPath, "index.html");
-            }
-
-            const readFileOutput = await readFile(HOSTING_BUCKET_NAME, subdomain, objectPath);
-            res.status(200);
-            res.setHeader("Content-Type", readFileOutput.contentType);
-            readFileOutput.body.pipe(res);
-        } else {
+    
+    async function validateUserSignedIn(req, res, next) {
+        const idToken = parseBearerToken(req.headers?.authorization);
+        if (!idToken){
+            res.status(401).send({error: "auth", message: "Expected a bearer token"});
+            return;
+        }
+    
+        try {
+            // TODO unit test this
+            const auth = getAuth(firebaseApp);
+            const decodedToken  = await auth.verifyIdToken(idToken);
+            req.userToken = decodedToken;
             next();
+        } catch(error){
+            res.status(401).send({error: "auth", message: "Invalid auth token"})
         }
-    } catch(error) {
-        if (error.name === "NoSuchKey") {
-            res.status(404).send("File does not exist");
+    }
+    
+    router.post('/create_site', validateUserSignedIn, async (req: Request, res) => {
+        // TODO add API contract somewhere as middleware, this validation is nuts
+        try {
+            // TODO replace this with specific origins
+            // validate the uploaded file is a zip file
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            validateSubdomain(req.body);
+            assertZipFile(req);
+    
+            // in zipFile, find the content root i.e the shallowest directory which contains an index.html file. 
+            // If no such directory exists then raise a ValidationError        
+            const zipFile = new AdmZip((req.files.zipFile as UploadedFile).data);
+            const contentRoot: string = inferContentRoot(zipFile);
+            console.log(`contentRoot: ${contentRoot}`);
+            
+            const subdomain = req.body.subdomain;
+            const bucketSiteUrl = `https://${subdomain}.litehost.io`;
+            const tmpDir = unzipToTmpDir(zipFile);
+            const uploadRoot = path.join(tmpDir, contentRoot);
+            await uploadDir(HOSTING_BUCKET_NAME, subdomain, uploadRoot, uploadRoot);
+            res.status(200).send({message: `Site created at ${bucketSiteUrl}`, websiteUrl: bucketSiteUrl});
+        } catch (error) {
+            if (error instanceof ValidationError){
+                res.status(error.status).send(error.body);
+            } else {
+                console.error(error);
+                res.status(500).send('An error occurred while creating the site');
+            }
+        }
+    });
+    
+    router.get('*', async(req, res, next) => {
+        try {
+            const subdomain = extractSubdomainFromHost(req.headers.host);
+            if (subdomain){
+                let objectPath = req.originalUrl.split("?")[0];
+                if (!isRequestForFile(objectPath) && objectPath.endsWith("/")){
+                    objectPath = path.join(objectPath, "index.html");
+                }
+    
+                const readFileOutput = await readFile(HOSTING_BUCKET_NAME, subdomain, objectPath);
+                res.status(200);
+                res.setHeader("Content-Type", readFileOutput.contentType);
+                readFileOutput.body.pipe(res);
+            } else {
+                next();
+            }
+        } catch(error) {
+            if (error.name === "NoSuchKey") {
+                res.status(404).send("File does not exist");
+            } else {
+                res.status(500).send("An unkonwn error happened");
+            }
+        }
+    });
+    
+    router.use("/", express.static("frontend"));
+    
+    function extractSubdomainFromHost(host: string) {
+        const url = new URL("http://" + host);
+        const hostname = url.hostname;
+        const parts = hostname.split('.');
+    
+        // contains exactly one subdomain
+        if (parts.length === 3 && parts[0] !== "www") {
+            return parts[0];
         } else {
-            res.status(500).send("An unkonwn error happened");
+            return null;
         }
     }
-});
-
-router.use("/", express.static("frontend"));
-
-function extractSubdomainFromHost(host: string) {
-    const url = new URL("http://" + host);
-    const hostname = url.hostname;
-    const parts = hostname.split('.');
-
-    // contains exactly one subdomain
-    if (parts.length === 3 && parts[0] !== "www") {
-        return parts[0];
-    } else {
-        return null;
+    
+    function isRequestForFile(path: string){
+        return /\.[^./]+$/.test(path);
     }
-}
-
-function isRequestForFile(path: string){
-    return /\.[^./]+$/.test(path);
-}
-
-function validateSubdomain(body: any){
-    const subdomain : string = body?.subdomain;
-    if (!subdomain) {
-        throw new ValidationError(
-            400, 
-            {
-                error: "invalid_request_body",
-                message: `Expected request body to contain subdomain. Request body: ${JSON.stringify(body)}`
-            }
-        );
+    
+    function validateSubdomain(body: any){
+        const subdomain : string = body?.subdomain;
+        if (!subdomain) {
+            throw new ValidationError(
+                400, 
+                {
+                    error: "invalid_request_body",
+                    message: `Expected request body to contain subdomain. Request body: ${JSON.stringify(body)}`
+                }
+            );
+        }
+    
+        if (!subdomain.match("^(?!-)[A-Za-z0-9-]{1,63}(?<!-)$")){
+            throw new ValidationError(
+                400,
+                {
+                    error: "invalid subdomain",
+                    message: "Domain must only contain alphanumeric characters, hyphens, and cannot start or end with a hyphen or period."
+                }
+            );
+        }
     }
-
-    if (!subdomain.match("^(?!-)[A-Za-z0-9-]{1,63}(?<!-)$")){
-        throw new ValidationError(
-            400,
-            {
-                error: "invalid subdomain",
-                message: "Domain must only contain alphanumeric characters, hyphens, and cannot start or end with a hyphen or period."
+    
+    function inferContentRoot(zipFile: AdmZip): string {
+            const entryPaths = listZipfileContents(zipFile);
+            let anyContentRootFound = false;
+            let shallowestDepth = Number.MAX_VALUE;
+            let shallowestDir = "";
+            let dirsAtThisDepth = 0;
+    
+            // first get all entries which have an index.html
+            entryPaths.filter(entryPath => entryPath.endsWith("index.html"))
+            // then keep track of # of entries at this depth
+            .forEach(entryPath => {
+                const entryDepth = entryPath.split('/').length - 1;
+                if (entryDepth < shallowestDepth) {
+                    shallowestDepth = entryDepth;
+                    shallowestDir = entryPath.replace(/\/?index\.html$/, '');
+                    anyContentRootFound = true;
+                    dirsAtThisDepth = 1;
+                } else if (entryDepth === shallowestDepth){
+                    dirsAtThisDepth++;
+                }
+            });
+    
+            if (!anyContentRootFound) {
+                throw new ValidationError(400, {
+                    error: "invalid_content_root",
+                    message: 'We could not find an index.html file in your uploaded zip file. Please upload a zip file containing an index.html'
+                });
             }
-        );
+    
+            if (dirsAtThisDepth > 1){
+                throw new ValidationError(400, {
+                    error: "ambiguous_content_root",
+                    message: 'Ambiguous content root. The uploaded zip file cannot have two index.html files at the same depths.'
+                })
+            }
+    
+            return shallowestDir;
     }
-}
-
-function inferContentRoot(zipFile: AdmZip): string {
-        const entryPaths = listZipfileContents(zipFile);
-        let anyContentRootFound = false;
-        let shallowestDepth = Number.MAX_VALUE;
-        let shallowestDir = "";
-        let dirsAtThisDepth = 0;
-
-        // first get all entries which have an index.html
-        entryPaths.filter(entryPath => entryPath.endsWith("index.html"))
-        // then keep track of # of entries at this depth
-        .forEach(entryPath => {
-            const entryDepth = entryPath.split('/').length - 1;
-            if (entryDepth < shallowestDepth) {
-                shallowestDepth = entryDepth;
-                shallowestDir = entryPath.replace(/\/?index\.html$/, '');
-                anyContentRootFound = true;
-                dirsAtThisDepth = 1;
-            } else if (entryDepth === shallowestDepth){
-                dirsAtThisDepth++;
-            }
-        });
-
-        if (!anyContentRootFound) {
+    
+    function assertZipFile(req){
+        if (! req.files || !req.files.zipFile){
             throw new ValidationError(400, {
-                error: "invalid_content_root",
-                message: 'We could not find an index.html file in your uploaded zip file. Please upload a zip file containing an index.html'
+                error: "no_files_uploaded",
+                message: "The input must contain exactly a single file"
             });
         }
-
-        if (dirsAtThisDepth > 1){
+    
+        if (req.files.zipFile.mimetype !== "application/zip"){
             throw new ValidationError(400, {
-                error: "ambiguous_content_root",
-                message: 'Ambiguous content root. The uploaded zip file cannot have two index.html files at the same depths.'
-            })
+                error: "file_not_zipfile",
+                message: "The uploaded file is not a zipfile. Please upload a zipfile."
+            });
         }
-
-        return shallowestDir;
-}
-
-function assertZipFile(req){
-    if (! req.files || !req.files.zipFile){
-        throw new ValidationError(400, {
-            error: "no_files_uploaded",
-            message: "The input must contain exactly a single file"
-        });
     }
-
-    if (req.files.zipFile.mimetype !== "application/zip"){
-        throw new ValidationError(400, {
-            error: "file_not_zipfile",
-            message: "The uploaded file is not a zipfile. Please upload a zipfile."
-        });
-    }
+    
+    
+    return {router, inferContentRoot, validateUserSignedIn};
 }
 
 
-
-// exports for tests
-export {inferContentRoot, validateUserSignedIn}; 
-
-export default router;
+export {createRouter};
